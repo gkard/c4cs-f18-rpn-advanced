715cd987465da6a435676123c9952cb5f65b7ee6
Gabriel Kardia <gkard@umich.edu>
2018-10-26 15:06:09 -0400

    making tests

--- rpn.py
+++ rpn.py
@@ -1,7 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 def calculate(arg):
-    pass
+    stack = [] 
+
+    tokens = arg.split()
+
+    for token in tokens:
+        try:
+            stack.append(int(token))
+        except ValueError:
+            val2 = stack.pop()
+            val1 = stack.pop()
+            if token == '+':
+                result = val1 + val2
+            elif token == '-':
+                result = val1 = val2
+
+            stack.append(result)
+
+    return stack[0]
+
 
 def main():
     while True:
