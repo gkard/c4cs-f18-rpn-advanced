6132f76df573559375ed0c157b4a0a8738761dd7
Gabriel Kardia <gkard@umich.edu>
2018-10-29 16:41:43 -0400

    added exponent operator

--- test_rpn.py
+++ test_rpn.py
@@ -15,3 +15,16 @@ class TestBasics(unittest.TestCase):
     def test_toomany(self):
         with self.assertRaises(ValueError):
             result = rpn.calculate('1 2 3 +')
+
+    def test_exp(self):
+        result = rpn.calculate('2 3 ^')
+        self.assertEqual(8, result)
+        result = rpn.calculate('10 4 ^')
+        self.assertEqual(10000, result)
+
+
+
+
+
+
+
